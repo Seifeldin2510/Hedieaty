485ce90af07ec28cff2c023eec63f28cf7432171
Seifeldin2510 <seifeldin.ashraf@yahoo.com>
2024-10-25 11:12:20 +0300

    home Page

---
+++ lib/View/gift_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GiftListPage extends StatefulWidget {
+  const GiftListPage({super.key});
+
+  @override
+  State<GiftListPage> createState() => _GiftListPageState();
+}
+
+class _GiftListPageState extends State<GiftListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
